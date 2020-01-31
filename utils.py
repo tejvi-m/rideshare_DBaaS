5550e31df81e003e3e3fb0def492cdc3d8037cd5
tejvi-m <tejvimstays@gmail.com>
2020-01-31 11:37:50 +0530

    added utils functiosn to check for validity of sha1 checksums

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import csv
+import hashlib
+
 
 def find_area(code):
     with open('data/AreaNameEnum.csv') as csv_file:
@@ -8,6 +10,20 @@ def find_area(code):
             if row[0] == code:
                 return True
 
+def hash(plaintext):
+    return hashlib.sha1(plaintext.encode()).hexdigest()
+
+def is_sha1(hash):
+    if len(hash) != 40:
+        return False
+    try:
+        sha = int(hash, 16)
+    except ValueError:
+        return False
+    return True
+
 if __name__ == '__main__':
-    area = input()
-    print(find_area(area))
+    # area = input()
+    # print(find_area(area))
+    print(is_sha1(hash("hello") + "1"))
+    print(hash("hello"))
