507aa27395d1a6690d72fce978a1e563fc8aed67
UmashankarTriforce <umashankarsivakumar@gmail.com>
2020-02-29 08:31:12 +0530

    bug fixes

--- servs/setup.sh
+++ servs/setup.sh
@@ -2,11 +2,11 @@
 
 if [ "$NAME" == "USER" ];
 then
-	mongod --fork --logpath /var/log/mongodb.log --bind_ip 172.16.238.10 
+	mongod --fork --logpath /var/log/mongodb.log --bind_ip 0.0.0.0 
 	python3 /code/db/setupUserDB.py
 	python3 /code/UserManagementAPIs.py
 else
-	mongod --fork --logpath /var/log/mongodb.log --bind_ip 172.16.238.11
+	mongod --fork --logpath /var/log/mongodb.log --bind_ip 0.0.0.0
 	python3 /code/db/setupRideDB.py
 	python3 /code/RideManagementAPIs.py
 fi
