7720b62bd51191cc25cb137d0995bf525dc70eb0
tejvi-m <tejvimstays@gmail.com>
2020-03-17 13:16:08 +0530

    minor fixes

--- Users/db/setupUserDB.py
+++ Users/db/setupUserDB.py
@@ -2,9 +2,9 @@ import pymongo
 import json as Json
 
 with open('/code/config.json') as json_file:
-  config = Json.load(config["MongoClientUser"])
+  config = Json.load(json_file)
 
-client = pymongo.MongoClient(config["MongoClientRides"])
+client = pymongo.MongoClient(config["MongoClientUser"])
 
 UserDB = client["UserDB"]
 users = UserDB["users"]
