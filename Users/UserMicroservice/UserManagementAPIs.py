f8b827630285a080021f2b3e2f76152bfac8a00b
Tejvi <41098307+tejvi-m@users.noreply.github.com>
2020-03-19 19:36:08 +0530

    setting threading to true

--- Users/UserMicroservice/UserManagementAPIs.py
+++ Users/UserMicroservice/UserManagementAPIs.py
@@ -316,5 +316,5 @@ def reset_count():
 if __name__ == '__main__':
 
 	app.debug=True
-	app.run('0.0.0.0', port = port)
+	app.run('0.0.0.0', port = port, threaded = True)
 	# app.run('127.0.0.1', port = 5000)
