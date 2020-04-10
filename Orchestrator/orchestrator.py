1ff7b52683a645c0a586fc60d8b11cd4ac3998da
tejvi-m <tejvimstays@gmail.com>
2020-04-10 23:13:44 +0530

    skeleton code for writes

--- Orchestrator/orchestrator.py
+++ Orchestrator/orchestrator.py
@@ -7,21 +7,29 @@ import json
 app = Flask(__name__)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('0.0.0.0', 5672))
-channel = connection.channel()
 
-channel.queue_declare(queue = "readQ")
+readChannel = connection.channel()
+writeChannel = connection.channel()
+
+readChannel.queue_declare(queue = "readQ")
 responseRPC = responseClient.ResponseQRpcClient("ResponseQ")
 
+writeChannel.queue_declare(queue = "writeQ")
 @app.route('/api/v1/db/read', methods=["POST"])
 def read():
 
-    # channel.basic_publish(exchange = "",
-    #                      routing_key = "readQ",
-    #                      body = json.dumps(request.get_json()))
-    # print("sent the request to the slave container")
-
     dataReturned = responseRPC.call(json.dumps(request.get_json()))
     return make_response(dataReturned, 200)
+
+@app.route('/api/v1/db/write', methods=["POST"])
+def write():
+    channel.basic_publish(exchange = "",
+                         routing_key = "writeQ",
+                         body = json.dumps(request.get_json()))
+    print("sent message")
+    return("hello", 200)
+
+
 
 if __name__ == '__main__':
 	app.debug=True
