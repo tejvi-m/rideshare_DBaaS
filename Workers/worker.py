67b52ac57acd5cbb07a64470bf27c8ef221be107
tejvi-m <tejvimstays@gmail.com>
2020-04-10 21:27:36 +0530

    worker connects to db to perform reads. terrible error handling

--- Workers/worker.py
+++ Workers/worker.py
@@ -1,19 +1,55 @@
 import pika
 import json
+import pymongo
+from flask import jsonify
+
+mClient = pymongo.MongoClient("0.0.0.0:27017")
+db = mClient["RideDB"]
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='0.0.0.0'))
 
 channel = connection.channel()
 
+
 channel.queue_declare(queue='ReadQ')
 
 def get_data(jsonData):
-    return "found"
+        req = json.loads(jsonData)
+        collection = db[req["collection"]]
+
+        if req["operation"] == "getNewRideID":
+                try:
+                    newRide = collection.find_one()["maxRideID"]
+                    return str(newRide + 1)
+                except:
+                    return "-1"
+
+        else:
+            match = req["data"]
+            selectFields = req["selectFields"]
+
+            try:
+                # not returning _id, plus having _id has problems with jsonify
+                records = collection.find(match, selectFields)
+
+                matches = {}
+                c = 0
+
+                for x in records:
+                    matches.update({c: x})
+                    c += 1
+
+                if c == 0:
+                     return ""
+
+                return json.dumps(jsonify(matches))
+            except:
+                return ""
 
 def on_request(ch, method, props, body):
     # jsonData = json.jsonify(body)
-    print("do i get here")
+    # print("do i get here")
     response = get_data(body)
 
 
