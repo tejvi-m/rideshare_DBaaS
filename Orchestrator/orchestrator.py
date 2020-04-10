d086c9b1804595ec1eff769fc6b45a66fd16e3bb
tejvi-m <tejvimstays@gmail.com>
2020-04-10 21:08:33 +0530

    using rpcs for reads through readQ and responseQ

--- Orchestrator/orchestrator.py
+++ Orchestrator/orchestrator.py
@@ -1,5 +1,6 @@
 import flask
 from flask import Flask, render_template, jsonify, request, abort, make_response, json
+import RPCClients.responseQClient as responseClient
 import pika
 import json
 
@@ -9,16 +10,18 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('0.0.0.0', 5672))
 channel = connection.channel()
 
 channel.queue_declare(queue = "readQ")
+responseRPC = responseClient.ResponseQRpcClient("ResponseQ1")
 
 @app.route('/api/v1/db/read', methods=["POST"])
 def read():
 
-    channel.basic_publish(exchange = "",
-                         routing_key = "readQ",
-                         body = json.dumps(request.get_json()))
-    print("sent the request to the slave container")
+    # channel.basic_publish(exchange = "",
+    #                      routing_key = "readQ",
+    #                      body = json.dumps(request.get_json()))
+    # print("sent the request to the slave container")
 
-    return make_response(jsonify({}), 200)
+    dataReturned = responseRPC.call(json.dumps(request.get_json()))
+    return make_response(dataReturned, 200)
 
 if __name__ == '__main__':
 	app.debug=True
